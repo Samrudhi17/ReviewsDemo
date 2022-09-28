fd6a02d98141974e075e4b1b13c6d3c5117c4def
Samrudhi17 <samrudhipawar15@gmail.com>
2022-09-29 00:47:36 +0530

    Added contact detail page

---
+++ app/src/main/java/com/product/reviewssystem/ContactDetail.kt
@@ -0,0 +1,11 @@
+package com.product.reviewssystem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+    }
+}
