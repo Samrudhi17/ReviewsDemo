5f972929edbd889006de0e11ec0a5fd8f58f27e1
Samrudhi17 <samrudhipawar15@gmail.com>
2022-09-28 18:45:33 +0530

    Added Intent

---
+++ app/src/main/java/com/product/reviewssystem/PersonalDetail.kt
@@ -0,0 +1,11 @@
+package com.product.reviewssystem
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_detail)
+    }
+}
