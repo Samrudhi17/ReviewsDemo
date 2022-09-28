8e34c41d67dcabb335fd10677818a4bdf44a4d40
Dhananjay <dhananjaypanage11@gmail.com>
2022-09-28 19:43:58 +0530

    First name and last name can only be typed on single line

--- app/src/main/java/com/product/reviewssystem/MainActivity.kt
+++ app/src/main/java/com/product/reviewssystem/MainActivity.kt
@@ -4,21 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btn1 :Button
-
+    private lateinit var textLayout : TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn1=findViewById(R.id.btn1)
-
-
+        textLayout = findViewById(R.id.firstnameET)
         btn1.setOnClickListener{
            startActivity(Intent(this,PersonalDetail::class.java))
         }
+
 
 
     }
