88b66953554d38ae38258a553f43d200a8a10c5a
Samrudhi17 <samrudhipawar15@gmail.com>
2022-09-28 19:41:51 +0530

    Added new intent for Email

--- app/src/main/java/com/product/reviewssystem/MainActivity.kt
+++ app/src/main/java/com/product/reviewssystem/MainActivity.kt
@@ -7,13 +7,13 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btn1 :Button
+    private lateinit var btn1 : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1=findViewById(R.id.btn1)
+        btn1 = findViewById(R.id.btn1)
 
 
         btn1.setOnClickListener{
